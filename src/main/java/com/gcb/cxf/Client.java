55d7cf73fd90e22f6a327020d0690011b41e6895
guvivi <guchenbo_work@sina.com>
2013-11-15 17:09:17 +0800

    Incomplete - task : 元模型API

--- src/main/java/com/gcb/cxf/Client.java
+++ src/main/java/com/gcb/cxf/Client.java
@@ -10,18 +10,18 @@ import javax.xml.ws.soap.SOAPBinding;
  * 
  */
 public class Client {
-	private static final QName SERVICE_NAME = new QName("http://cxf.gcb.com",
+	private static final QName SERVICE_NAME = new QName("http://cxf.gcb.com/",
 			"HelloWorld");
-	private static final QName PORT_NAME = new QName("http://cxf.gcb.com",
+	private static final QName PORT_NAME = new QName("http://cxf.gcb.com/",
 			"HelloWorldPort");
 
 	public static void main(String[] args) {
 		System.out.println("Start Client");
 		Service service = Service.create(SERVICE_NAME);
-		String endpointAddress = "http://localhost:9000/HelloWorld?wsdl";
+		String endpointAddress = "http://localhost:9000/HelloWorld";
 		service.addPort(PORT_NAME, SOAPBinding.SOAP11HTTP_BINDING,
 				endpointAddress);
-		HelloWorld hw = service.getPort(HelloWorld.class);
+		HelloWorld hw = service.getPort(PORT_NAME, HelloWorld.class);
 		String str = hw.sayHello("demo");
 		System.out.println(str);
 	}
---
+++ src/main/java/com/gcb/domain/Employee.java
@@ -0,0 +1,46 @@
+package com.gcb.domain;
+
+import java.io.Serializable;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable {
+	private static final long serialVersionUID = -7061118034132158101L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected Long id;
+
+	private String name;
+
+	private Long salary;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+}
